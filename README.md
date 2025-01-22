5845846b6a482fe7df00aad8324a1981e222df84
MelchorRuiz <me@melchor-ruiz.fr>
2025-01-22 19:46:56 +0000

    feat: Part 1

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Ditherpunk
+
+## Question 2
+
+**On obtient un DynamicImage, à quoi correspond ce type?**<br>
+Rgba8
+
+**Comment obtenir une image en mode rbg8 à partir de ce DynamicImage?**<br>
+```img.to_rgb8();```
+
+## Question 3
+**Que se passe-t-il si l’image de départ avait un canal alpha?**<br>
+Tous les pixels transparents deviennent noirs, comme le montre l'exemple suivant
+
+| Avant | Après | 
+| :-: | :-: |
+| ![avant](./img/gato.png) | ![apres](./img/gato_rgb.png) |
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use image::ImageError;
+use image::Rgb;
+use image::io::Reader as ImageReader;
+
+fn main() -> Result<(), ImageError> {
+    let img = ImageReader::open("img/gato.png")?.decode()?;
+    println!("type: {:?}", img.color());
+    let mut img_rgb = img.to_rgb8(); 
+    img_rgb.save("img/gato_rgb.png")?;
+
+    println!("(32, 52) {:?}", img_rgb.get_pixel(32, 52));
+    
+    for (x, y, pixel) in img_rgb.enumerate_pixels_mut() {
+        if (x + y) % 2 == 0 {
+            *pixel = Rgb([255, 255, 255]);
+        }
+    }
+
+    img_rgb.save("img/gato_rgb_white.png")?;
+
+    Ok(())
 }
